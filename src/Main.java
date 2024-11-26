e2af5098cde835a9fb8fe2a74eef0d5424f1f9c6
Amandickz <amandickz@gmail.com>
2024-11-26 10:17:39 -0300

    Primeiro exercício realizado

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner scan = new Scanner(System.in);
 		
-
+		int a, b, result;
+		
+		System.out.printf("Digite o primeiro número: ");
+		a = scan.nextInt();
+		
+		System.out.printf("Digite o primeiro número: ");
+		b = scan.nextInt();
+		
+		result = a + b;
+		
+		System.out.printf("O resultado da soma é: %d", result);
 	}
 
 }
